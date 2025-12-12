3d45dbb91fcf9c45281dae8f35b2f1aa36b6a4d6
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-12 22:15:22 +0530

    Even Odd java program

---
+++ src/com/evenodd/Task1EvenOdd.java
@@ -0,0 +1,35 @@
+package com.evenodd;
+
+public class Task1EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int number=43;
+       {
+    	if(number%2==0)   {
+       
+       System.out.println("number is even"+number);
+       }
+    	else{
+    		System.out.println("number is odd"+number);
+    	}
+    	
+    	switch (number) {
+    	case 1:
+    		System.out.println("number is even"+number);
+    		break;
+    	case -1:
+    		System.out.println("number is odd"+number);
+    		break;
+    		
+    			
+    	
+    }
+   }
+  }
+ }
+
+	
+
+
+
